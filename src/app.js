55077be671f432eb081df6253568139b131e7463
Timothy Ford <71365012+tsfordcoding@users.noreply.github.com>
2023-06-23 20:26:24 -0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const pastes = require("./data/pastes-data");
+
+// TODO: Follow instructions in the checkpoint to implement ths API.
+
+app.use(express.json());
+
+app.use("/pastes/:pasteId", (req, res, next) => {
+  const { pasteId } = req.params;
+  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+
+  if (foundPaste) {
+    res.json({ data: foundPaste });
+  } else {
+    next(`Paste id not found: ${pasteId}`);
+  }
+});
+
+app.get("/pastes", (req, res) => {
+  res.json({ data: pastes });
+});
+
+// Variable to hold the next ID
+let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
+
+app.post("/pastes", (req, res, next) => {
+  console.log(req.body);
+  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } =
+    req.body;
+
+  if (text) {
+    const newPaste = {
+      id: ++lastPasteId, // Increment last ID, then assign as the current ID
+      name,
+      syntax,
+      exposure,
+      expiration,
+      text,
+      user_id,
+    };
+    pastes.push(newPaste);
+    res.status(201).json({ data: newPaste });
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+// Not found handler
+app.use((request, response, next) => {
+  next(`Not found: ${request.originalUrl}`);
+});
+
+// Error handler
+app.use((error, request, response, next) => {
+  console.error(error);
+  response.send(error);
+});
+
+module.exports = app;
